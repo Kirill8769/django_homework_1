5544012556ce7aa9bba7b4ef1adfc84670d1fd18
kirill8769 <tkkrozn@gmail.com>
2024-02-29 22:09:58 +0300

    finished homework 1

---
+++ src/main.py
@@ -0,0 +1,47 @@
+import os
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    """
+    Специальный класс, который отвечает за
+    обработку входящих запросов от клиентов
+    """
+
+    @staticmethod
+    def __get_html_content():
+        html_path = os.path.join("../templates", "index.html")
+        print(os.path.isfile(html_path))
+        with open(html_path, encoding="utf-8") as file:
+            html_page = file.read()
+        return html_page
+
+    def do_GET(self):
+        """ Метод для обработки входящих GET-запросов """
+
+        query_components = parse_qs(urlparse(self.path).query)
+        print(query_components)
+
+        page_content = self.__get_html_content()
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
